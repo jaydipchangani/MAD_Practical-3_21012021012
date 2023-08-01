75a001feb78a8d9c45bdc7173f28b426e58540d3
student <student@gnu.ac.in>
2023-08-01 08:56:11 +0530

    Practical-3

---
+++ app/src/main/java/com/example/mad_practical_3_21012021012/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_practical_3_21012021012
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/test/java/com/example/mad_practical_3_21012021012/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.mad_practical_3_21012021012
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
